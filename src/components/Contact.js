1e0ed99b3d835132c96fbd96fe20ce027c52bf3a
Dylan Cheng <68773823+dylncheng@users.noreply.github.com>
2022-01-29 02:34:48 -0500

    changed email in Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,7 +6,7 @@ const Contact = () => {
         <div className="contact" id="contact">
             <h2>Links</h2>
             <ul>
-                <li className="email"><a href={"mailto:chengdylan02@gmail.com"} target="_blank">email</a></li>
+                <li className="email"><a href={"mailto:dylan.cheng@mail.utoronto.ca"} target="_blank">email</a></li>
                 <li className="linkedin"><a href={"https://linkedin.com/in/dylnchng"} target="_blank">linkedin</a></li>
                 <li className="github"><a href={"https://github.com/dylncheng"} target="_blank">github</a></li>
             </ul>
